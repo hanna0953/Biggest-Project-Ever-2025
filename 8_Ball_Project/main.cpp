5b1df4bb3b6166c26f0397a87d7b3775c68f92ec
bludan <bluzzenith@gmail.com>
2024-12-21 00:13:23 +0100

    This is a random number generator

--- 8_Ball_Project/main.cpp
+++ 8_Ball_Project/main.cpp
@@ -5,24 +5,11 @@
 using namespace std;
 
 int main() {
+
   srand(time(0));
-<<<<<<< HEAD
   int randomNum = rand() % 20 + 1;
   string user_Input;
   
-=======
-
-  ifstream file("answers.txt"); // Open the file 
-  if (!file) 
-  { 
-    cerr << "Unable to open file answers.txt\n"; 
-    return 1; // Exit if the file cannot be opened 
-  } 
-
-  string line;
-  int current_Line = 0;
-
->>>>>>> refs/remotes/origin/main
   cout << "\nWelcome to 8Ball!\n\nPlease write your question here: ";
   cin >> user_Input;
 
@@ -34,7 +21,9 @@ int main() {
           cout << "Answer: " << line << endl; 
           break; // Exit the loop once the desired line is found 
       } 
-   } 
+   }
+  
+   
   return 0;
   //test123
 } 
