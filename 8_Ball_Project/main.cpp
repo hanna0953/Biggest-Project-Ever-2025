c99fe043d09f25ad6a5c1816c1d3c211c704ccbd
bludan <bluzzenith@gmail.com>
2024-12-20 23:42:13 +0100

    Random number generator in randomNum

--- 8_Ball_Project/main.cpp
+++ 8_Ball_Project/main.cpp
@@ -4,10 +4,8 @@ using namespace std;
 
 int main() {
   string user_Input;
-
-  int randomNum = rand() % 21;
-  
   srand(time(0));
+  int randomNum = rand() % 20 + 1;
 
   cout << "Welcome to 8Ball!\nPlease write your question here: ";
   cin >> user_Input;
