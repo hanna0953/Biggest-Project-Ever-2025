ca52f2f546a77f0857ee3125ee46f556daeec7e3
bludan <bluzzenith@gmail.com>
2024-12-22 00:16:17 +0100

    Pulls a random word from the fiveletterwords.txt dataset

--- Wordle/main.py
+++ Wordle/main.py
@@ -1,10 +1,15 @@
-from pathlib import Path
 # Data for the five letter words has been aquired from previous WORDLE games
+from pathlib import Path
+import random
+
 user_Player_Count = 0
 path_To_Words = Path(__file__).with_name('fiveletterwords.txt')
 with path_To_Words.open('r') as words:
-    total_Word_Count = len(words.readlines())
-    print(total_Word_Count)
-# while 1:
-# print(total_Word_Count)
+    content = words.readlines()
+    total_Word_Count = len(content)
+    random_Number = random.randint(0, total_Word_Count - 1)
+    print (content[random_Number])
+    words.close()  
+
+
 
