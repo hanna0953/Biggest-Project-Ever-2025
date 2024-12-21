3710882cad7bfd29f330d53eb097697b7857128c
hann <hanna.ilnicka@gmail.com>
2024-12-22 00:14:37 +0100

    Update to the user input restrictions, removed protection from putting numbers and signs to user input for better future build

--- Wordle/main.py
+++ Wordle/main.py
@@ -13,10 +13,21 @@ with path_To_Words.open('r') as words:
 test_Word = ['BREAD']
 print("\nWelcome to WORDLE!\nYour word: _ _ _ _ _\n\n")
 
-user_Word = input("Write your guess here: ")
-if user_Word == 5:
-    print("Your guess: " + user_Word)
-elif not re.match(r"^[A-Za-z]+$", user_Word):
-    print("\nWrong input, please write only letters A-Z\n")
-else:
-    print("\nWrong input, your word should be 5 letter long!\n")
+# user_Word = input("Write your guess here: ")
+# if user_Word == 5:
+#     print("Your guess: " + user_Word)
+# elif not re.match(r"^[A-Za-z]+$", user_Word):
+#     print("\nWrong input, please write only letters A-Z\n")
+# else:
+#     print("\nWrong input, your word should be 5 letter long!\n")
+
+while True:
+    try:
+        user_word = input('Write your guess here: ')
+        if len(user_word) != 5:
+            raise ValueError
+        break 
+    except ValueError:
+        print("\nWrong input, your word should be 5 letters long!\n")
+
+#list(user_Word)
