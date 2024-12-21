7e636063a6a7b7211c37f7337ee26fdb3ca4d13a
bludan <bluzzenith@gmail.com>
2024-12-21 23:30:30 +0100

    Get the number of words

--- Wordle/main.py
+++ Wordle/main.py
@@ -1,2 +1,10 @@
+from pathlib import Path
 # Data for the five letter words has been aquired from previous WORDLE games
+user_Player_Count = 0
+path_To_Words = Path(__file__).with_name('fiveletterwords.txt')
+with path_To_Words.open('r') as words:
+    total_Word_Count = len(words.readlines())
+    print(total_Word_Count)
+# while 1:
+# print(total_Word_Count)
 
